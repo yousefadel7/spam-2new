6bac1498a687e9fec63c47872dfe33fc746737e0
yousefadel7 <46754762+yousefadel7@users.noreply.github.com>
2019-01-16 19:54:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ client2.on('message', message => {
 });
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === ')spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -68,7 +68,7 @@ if (message.content === '!spam') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === ')spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
